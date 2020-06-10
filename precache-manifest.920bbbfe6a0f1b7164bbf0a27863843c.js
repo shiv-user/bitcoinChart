152dfe42f62a95988e4048d22d29d3a28ad6433c
shiv-user <shivranjan1497@gmail.com>
2020-06-10 21:02:39 +0530

    Updates

---
+++ precache-manifest.920bbbfe6a0f1b7164bbf0a27863843c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3396775f8f56877fd3978d64fd6c0c6",
+    "url": "/bitcoinChart/index.html"
+  },
+  {
+    "revision": "3f66e07c3a9bedf0bb70",
+    "url": "/bitcoinChart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e49a4e9a201e31c72e98",
+    "url": "/bitcoinChart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f66e07c3a9bedf0bb70",
+    "url": "/bitcoinChart/static/js/2.9ab35794.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/bitcoinChart/static/js/2.9ab35794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e49a4e9a201e31c72e98",
+    "url": "/bitcoinChart/static/js/main.3344d9cf.chunk.js"
+  },
+  {
+    "revision": "2d344d39b4b652e02c34",
+    "url": "/bitcoinChart/static/js/runtime-main.bb9ad048.js"
+  }
+]);
